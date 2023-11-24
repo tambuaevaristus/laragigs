3a22ee7de0eeb5a89c5ecef152331b26c7711b89
Tambua Evaristus <evaristustambua@gmail.com>
2023-11-24 15:11:35 +0100

    user controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ListingController::class,'index']);
-Route::get('/listings/create', [ListingController::class,'create']);
-Route::post('/listings', [ListingController::class,'store']);
-Route::get('/listings/{id}', [ListingController::class,'show']);
+Route::get('/', [ListingController::class, 'index']);
+Route::get('/listings/create', [ListingController::class, 'create']);
+Route::post('/listings', [ListingController::class, 'store']);
+Route::get('/listings/{id}', [ListingController::class, 'show']);
+
+
+
+// USer routes 
+
+Route::post('/user', [UserController::class, 'create']);
